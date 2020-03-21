406944e074c2494ac20612ad6b1cc8dd6cf38af0
AleksandrStefanovich <rodger6699@gmail.com>
2020-03-21 16:15:54 +0300

    /

--- src/by/academy/HomeWork1/CalculatorWithOperator.java
+++ src/by/academy/HomeWork1/CalculatorWithOperator.java
@@ -1,4 +1,36 @@
 package by.academy.HomeWork1;
 
+import static java.lang.StrictMath.pow;
+
 public class CalculatorWithOperator {
+   private double result;
+    public double divide(double a, double b){
+        result = a / b;
+       return result;
+   }
+    public double multiply(double a, double b){
+        result = a * b;
+        return result;
+    }
+    public double add(double a, double b){
+        result = a + b;
+        return result;
+    }
+    public double subtract(double a, double b){
+        result = a - b;
+        return result;
+    }
+    public double modulus(double a){
+        result = Math.abs(a);
+        return result;
+    }
+    public double square(double base, double exponent){
+       result = pow(base, exponent);
+        return result;
+    }
+    public double sqrt(double base, double exponent){
+        result = pow(base,(double)1/exponent);
+        return result;
+    }
+
 }
--- src/by/academy/HomeWork1/Main.java
+++ src/by/academy/HomeWork1/Main.java
@@ -1,5 +1,11 @@
 package by.academy.HomeWork1;
 
 public class Main {
+    public static void main(String[] args) {
+        CalculatorWithOperator calc = new CalculatorWithOperator();
+        float result;
+        result = (float)calc.add((calc.add(calc.square(calc.divide(28, 5),2),calc.multiply(15, 7))),4.1);
+        System.out.println(result);
+    }
 
 }
