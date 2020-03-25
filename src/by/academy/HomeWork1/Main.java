ec8d3f37b53d3dc413bda60679da43f57848c90d
Aleksandr Stefanovich <rodger6699@gmail.com>
2020-03-25 12:50:13 +0300

    /

--- src/by/academy/HomeWork1/Main.java
+++ src/by/academy/HomeWork1/Main.java
@@ -6,14 +6,15 @@ public class Main {
         CalculatorWithOperator calc = new CalculatorWithOperator();
         float result;
 
-
-
-
         // 4.1 + 15 * 7 + (28 / 5) ^ 2 вывод в result
         // первым выполняется деление, потом возведение в квадрат, сложение с результатом произведения и последним сложение с 4.1
+        // дробную степень например 2/5 можно реализовать как calc.sqrt(calc.squre(n,2),5)
+        //calc.sqrt считает корень любой степени с точностью до 3 знаков после запятой
 
         result = (float)calc.add((calc.add(calc.square(calc.divide(28, 5),2),calc.multiply(15, 7))),4.1);
         System.out.println(result);
-        System.out.println(calc.sqrt(290,2));
+        System.out.println(calc.sqrt(4096,2));
+
+        
     }
 }
