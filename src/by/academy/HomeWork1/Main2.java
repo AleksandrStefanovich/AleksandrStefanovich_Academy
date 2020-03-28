f49ab8e9f91c8e246ac07c01d1fdab37bae7c045
Aleksandr Stefanovich <rodger6699@gmail.com>
2020-03-28 23:29:12 +0300

    /

--- src/by/academy/HomeWork1/Main2.java
+++ src/by/academy/HomeWork1/Main2.java
@@ -8,7 +8,7 @@ public class Main2 {
         ICalculator calc2 = new CalculatorWithCounter(new CalculatorWithOperator());
         result1 = calc1.add((calc1.add(calc1.square(calc1.divide(28, 5),2),calc1.multiply(15, 7))),4.1);
         result2 = calc2.add((calc2.add(calc2.square(calc2.divide(28, 5),2),calc2.multiply(15, 7))),4.1);
-        System.out.println(result1 +" in "+ ((CalculatorWithCounter) calc1).getCountOperation()+" operations.");
-        System.out.println(result2 +" in "+ ((CalculatorWithCounter) calc2).getCountOperation()+" operations.");
+        System.out.println("Result is "+result1 +" in "+ ((CalculatorWithCounter) calc1).getCountOperation()+" operations.");
+        System.out.println("Result is "+result2 +" in "+ ((CalculatorWithCounter) calc2).getCountOperation()+" operations.");
     }
 }
