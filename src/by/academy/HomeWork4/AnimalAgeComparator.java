5af68687cfc7d2d18cb7be0feb22fba19fa25d4b
Alexander <rodger6699@gmail.com>
2020-04-10 00:45:15 +0300

    .

--- src/by/academy/HomeWork4/AnimalAgeComparator.java
+++ src/by/academy/HomeWork4/AnimalAgeComparator.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class AnimalAgeComparator implements Comparator<Animal> {
     @Override
     public int compare(Animal o1, Animal o2) {
-        if (o1.getAge() == o2.getAge()) {
+        if (o1.getAge() == o2.getAge()) { //0 если одинаковый, 1 если 1 старше, -1 если 2 старше
             return 0;
         } else if(o1.getAge() > o2.getAge()) {
         return 1;
--- src/by/academy/HomeWork4/DataContainer.java
+++ src/by/academy/HomeWork4/DataContainer.java
@@ -1,5 +1,6 @@
 package by.academy.HomeWork4;
 
+import javax.xml.crypto.Data;
 import java.util.Arrays;
 
 public class DataContainer<E> {
@@ -15,6 +16,29 @@ public class DataContainer<E> {
        this.item = Arrays.copyOf(this.item, this.item.length + 1);
        this.item[this.item.length - 1] = item;
    }
+
+   public static void sort(DataContainer<Person> item, PersonRegistrationComparator comp){
+       Person tempContainer = new Person(null,null,null);
+       for (int i = 0; i < item.getData().length; i++) {
+           switch (comp.compare(item.getData()[i],item.getData()[i+1])){
+               case 0: {
+                   break;}
+               case 1: {
+                  tempContainer =  item.getData()[i];
+                  item.getData()[i] = item.getData()[i+1];
+                  item.getData()[i+1] = tempContainer;
+                   break;}
+               case -1: {
+                   tempContainer =  item.getData()[i+1];
+                   item.getData()[i+1] = item.getData()[i];
+                   item.getData()[i] = tempContainer;
+                   break;}
+           }
+       }item.getData();
+
+   }
+
+   public static void sort(DataContainer<Animal> item , AnimalAgeComparator comp){}
 
    public E[] getData(){
         return item;
